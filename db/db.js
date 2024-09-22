298b8a053fa96782fce3b34f1cfba8986b036075
Zahar <crtrever@gmail.com>
2024-09-22 19:37:37 +0300

    Fix2

--- db/db.js
+++ db/db.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 const DB_URI = process.env.DB_URI;
 
 mongoose
-  .connect(DB_URI)
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    tlsInsecure: true,
+  })
   .then(() => {
     console.log("Database connection successful");
   })
