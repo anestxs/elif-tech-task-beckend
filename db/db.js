3ce639a08e6a93495a914484ba7f40515a807421
Zahar <crtrever@gmail.com>
2024-09-22 19:51:10 +0300

    Fix3

--- db/db.js
+++ db/db.js
@@ -4,8 +4,6 @@ const DB_URI = process.env.DB_URI;
 
 mongoose
   .connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     ssl: true,
     tlsInsecure: true,
   })
--- routes/participantsRouter.js
+++ routes/participantsRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createParticipant } from "../controllers/participantsControllers.js";
+import {
+  createParticipant,
+  getAllParticipants,
+} from "../controllers/participantsControllers.js";
 
 const participantsRouter = express.Router();
 const jsonParser = express.json();
 
-participantsRouter.get("/");
+participantsRouter.get("/", getAllParticipants);
 participantsRouter.post("/register/:id", jsonParser, createParticipant);
 
 export default participantsRouter;
